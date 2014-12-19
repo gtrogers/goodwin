450f81dc58ad57ca8c9e78437e4b9e8fafff3f4b
Gareth <grogers@thoughtworks.com>
2014-12-19 22:09:42 +0000

    Fix matcher tests

--- src/goodwin/mappers/services.clj
+++ src/goodwin/mappers/services.clj
@@ -15,13 +15,19 @@
               :status (service-status text)}))
          service-statuses)))
 
-(def string-matcher (fn [service] (.contains (:text service) description)))
-(def regex-matcher  (fn [service] (re-find description (:text service))))
+(defn- string-matcher [string]
+  (fn [service] (.contains (:text service) string)))
+
+(defn- regex-matcher [regex]
+  (fn [service] (re-find regex (:text service))))
+
+(defn- regex? [thing]
+  (instance? java.util.regex.Pattern thing))
 
 (defn service-matching [description services]
   (let [matcher (cond
-                  (string? description)                           string-matcher 
-                  (instance? java.util.regex.Pattern description) regex-matcher
+                  (string? description) (string-matcher description) 
+                  (regex? description)  (regex-matcher description)
                   :else (throw (IllegalArgumentException. "Expecting string or regex pattern.")))]
   (first (filter matcher services))))
 
