4b94a1b68ff74144fb4479d8873a8357d5915196
Gareth <grogers@thoughtworks.com>
2014-12-19 18:07:46 +0000

    Add function for performing multiple commands using a single session

--- src/goodwin/ssh_commands.clj
+++ src/goodwin/ssh_commands.clj
@@ -7,8 +7,19 @@
     (session agent ip {:username user
                        :strict-host-key-checking :no})))
 
-(def ^:private services-command
-  (fn [session] (ssh session {:cmd "/sbin/service --status-all"})))
+(defn- build-command-fn [cmd-string]
+  (fn [session] (ssh session {:cmd cmd-string})))
+
+(def ^:private services-command {:name :services
+                                 :cmd (build-command-fn "/sbin/service --status-all")})
+
+(defn SSH>
+  "Attempts to run the commands provided and returns
+  a map of command-names to command results"
+  [session & commands]
+  (with-connection session
+    (reduce (fn [result command]
+              (assoc result (:name command) ((:cmd command) session))) {} commands)))
 
 (defn services-status [ip user key-path]
   (let [session (get-session ip user key-path)]
